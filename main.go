28910c061933bd8beb503627023d77209031fb80
Yann Salaün <yannsalaun1@gmail.com>
2015-12-02 20:43:32 +0100

    Add main.go

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	flag.Parse()
+	name := flag.Arg(0)
+	if name == "" {
+		fmt.Println("usage:", os.Args[0], "name [args...]")
+		os.Exit(1)
+	}
+	for {
+		cmd := exec.Command(name, flag.Args()[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		err := cmd.Start()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = cmd.Wait()
+		if err != nil {
+			log.Print(err)
+			time.Sleep(time.Second)
+		} else {
+			break
+		}
+	}
+}
