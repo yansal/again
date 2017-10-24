80eb54e9f03a13529bcd19888a82190b8a855e06
Yann Salaün <yannsalaun1@gmail.com>
2017-10-24 12:13:23 +0200

    Update main.go

--- main.go
+++ main.go
@@ -9,15 +9,18 @@ import (
 )
 
 func main() {
+	log.SetFlags(log.Lshortfile)
+	log.SetPrefix("again: ")
+
 	fail := flag.Bool("fail", false, "run until command fails")
 	flag.Parse()
-	name := flag.Arg(0)
-	if name == "" {
-		fmt.Println("usage:", os.Args[0], "name [args...]")
+	cmdname := flag.Arg(0)
+	if cmdname == "" {
+		fmt.Println("usage:", "again cmdname [cmdargs...]")
 		os.Exit(1)
 	}
 	for {
-		cmd := exec.Command(name, flag.Args()[1:]...)
+		cmd := exec.Command(cmdname, flag.Args()[1:]...)
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
